4c2c46afa14a58b953edb6b05398f69f5edcf153
Tommy Grullón Contreras <elnegritodepapi@hotmail.com>
2023-08-11 16:11:51 -0400

    Added routes to manage post requests.

--- config/app.php
+++ config/app.php
@@ -163,7 +163,8 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
+        Barryvdh\Debugbar\ServiceProvider::class,
+         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,39 @@
 <?php
 
+use App\Http\Controllers\FallBackController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
+use Barryvdh\Debugbar\Facades\Debugbar;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+    GET - Request a resource
+    POST - Create a new resource
+    PUT - Update a resource
+    PATCH - Modify a resource
+    DELETE - Delete a resource
+    OPTIONS - Ask the server wich verbs are allowed
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('blog')->group(function () {
+    // GET
+    Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+    Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show');
+
+    // POST
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+    Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+
+    // PUT OR PATCH 
+    Route::get('edit/{id}', [PostsController::class, 'edit'])->name('blog.edit');
+    Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update');
+
+    // DELETE
+    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.destroy');
 });
+
+// Route for invoke method
+Route::get('/', HomeController::class);
+
+// Fallback Route
+Route::fallback(FallBackController::class);

91f016b4bcb46fdfda0907ad6acfe3dd74535e84
Tommy Grullón Contreras <tommycontreras34@gmail.com>
2023-08-14 23:06:26 -0400

    Added middleware validation to restrict access

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth')->only(['create', 'edit', 'update', 'destroy']);    
+    }
+
     /**
      * Display a listing of the resource.
      */
